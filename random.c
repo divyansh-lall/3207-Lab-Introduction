5d7095f1878c7115bb7851c213207f14c924ba29
divyansh-lall <tur48174@temple.edu>
2025-08-27 14:13:00 -0400

    Update random.c

--- random.c
+++ random.c
@@ -1,16 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-char randchar();  
-
-int main() {
-    srand(time(NULL));
-    char word[8];
-    for (int i = 0; i < 7; i++) {
-        word[i] = randchar();
-    }
-    word[7] = '\0';
-    printf("Random 7-letter word: %s\n", word);
-    return 0;
+char randchar() {
+    return 'A' + (rand() % 26);
 }
